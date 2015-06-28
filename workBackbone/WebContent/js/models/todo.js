982187e294dd30ebae1e176dbefc49a9183ef21c
k_wachi <k_wachi@icloud.com>
2015-06-28 15:09:42 +0900

    20150628

--- workBackbone/WebContent/js/models/todo.js
+++ workBackbone/WebContent/js/models/todo.js
@@ -1,3 +1,16 @@
 /**
- * 
+ * js/models/todo.js
  */
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+	defaults: {
+		title: '',
+		completed: false
+	},
+	toggle: function(){
+		this.save({
+			completed: !this.get('completed')
+		});
+	}
+});
--- workBackbone/WebContent/js/views/app.js
+++ workBackbone/WebContent/js/views/app.js
@@ -1,3 +1,10 @@
 /**
- * 
+ * js/views/app.js
  */
+var app = app || {};
+
+app.AppView = Backbone.View.extend({
+	el: '#todoapp',
+	
+});
+
